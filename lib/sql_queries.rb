76b088b87f3400fc9c962d5c7f7c91bd022b70c9
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-29 01:43:02 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,10 +8,10 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, SUM(pledges.amount) 
-  FROM projects, 
-  INNER JOIN pledges, 
+  FROM projects
+  INNER JOIN pledges 
   ON projects.id = pledges.projects_id,
-  GROUP BY project.id,
+  GROUP BY project.id
   ORDER BY projects.title;"
   
 end
