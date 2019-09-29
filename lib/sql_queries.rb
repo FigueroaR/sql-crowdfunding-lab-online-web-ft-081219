d20b1806431c73344dafeba0b9f9baf6d76a5c54
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-29 02:39:32 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,10 +27,10 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.tilte, SUM(pledges.amount) - projects.funding_goal
-  FROM projects
-  INNER JOIN pledges
-  ON projects.id = pledges.project_id; HAVING SUM(pledges.amount) >= pledges.funding_goal;"
-end
+    FROM projects
+    INNER JOIN pledges
+    ON projects.id = pledges.project_id; HAVING SUM(pledges.amount) >= pledges.funding_goal;"
+  end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "Write your SQL query Here"
